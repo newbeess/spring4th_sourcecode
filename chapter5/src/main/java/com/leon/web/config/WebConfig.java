ae90258669c9b5e94cfdfaee6d47833f0b70553c
newbeess <newbeessfu@gmail.com>
2018-10-04 01:27:04 +0800

    stash

--- chapter5/src/main/java/com/leon/web/config/WebConfig.java
+++ chapter5/src/main/java/com/leon/web/config/WebConfig.java
@@ -1,5 +1,14 @@
 package com.leon.web.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(value = "com.leon.web")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
 
 }
--- chapter5/src/main/java/com/leon/web/controller/HomeController.java
+++ chapter5/src/main/java/com/leon/web/controller/HomeController.java
@@ -1,5 +1,8 @@
 package com.leon.web.controller;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class HomeController {
 
 }
